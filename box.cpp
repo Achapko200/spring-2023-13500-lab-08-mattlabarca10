3f577fc809d936db94d04c7563ea28deef03acac
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2023-05-08 08:04:10 -0400

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -53,3 +53,28 @@ int frame(){
   
   return 0;
 }
+int half(){
+  std::string input = "image2.pgm";
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); 
+  // read it from the file "image2.pgm"
+  // h and w were passed by reference and now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  
+  // Now we can invert the right half of the picture
+  int out[MAX_H][MAX_W];
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++){
+        if(col >= w/2)
+            out[row][col] = (255-img[row][col]);
+        else
+            out[row][col] = img[row][col];
+    }
+  }
+  
+  // and save this new image to file "taskB.pgm"
+  writeImage("taskB.pgm",out, h, w);
+  
+  return 0;
+}
