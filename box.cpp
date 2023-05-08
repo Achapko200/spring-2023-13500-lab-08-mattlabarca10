efa4452c041309346452c86c28185fc5d3750939
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2023-05-08 08:15:08 -0400

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -3,16 +3,6 @@
 #include "imageio.h"
 
 int box(){
-  std::string input = "image1.pgm";
-  int img[MAX_H][MAX_W];
-  int h, w;
-  readImage(input, img, h, w); 
-  // read it from the file "image1.pgm"
-  // h and w were passed by reference and now contain the dimensions of the picture
-  // and the 2-dimesional array img contains the image data
-  
-  // Now we can invert the image
-  int out[MAX_H][MAX_W];
   for(int row = 0; row < h; row++) {
     for(int col = 0; col < w; col++) {
       if(col < w/4 || col >= 3*w/4 || row < h/4 || row >= 3*h/4)
@@ -28,16 +18,6 @@ int box(){
   return 0;
 }
 int frame(){
-  std::string input = "image1.pgm";
-  int img[MAX_H][MAX_W];
-  int h, w;
-  readImage(input, img, h, w); 
-  // read it from the file "image1.pgm"
-  // h and w were passed by reference and now contain the dimensions of the picture
-  // and the 2-dimesional array img contains the image data
-  
-  // Now we can invert the image
-  int out[MAX_H][MAX_W];
   for(int row = 0; row < h; row++) {
     for(int col = 0; col < w; col++) {
       if((col == w/4 || col == 3*w/4 || row == h/4 || row == 3*h/4)&&(col >= w/4 && col <= 3*w/4 && row >= h/4 && row <= 3*h/4)){
