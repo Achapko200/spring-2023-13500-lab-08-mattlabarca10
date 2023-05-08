f42c54992a08065dd497466dc41d2f21faa57c1a
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2023-05-08 08:15:34 -0400

    Update box.h

--- box.h
+++ box.h
@@ -1,5 +1,16 @@
 #pragma once
 
+std::string input = "image1.pgm";
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); 
+  // read it from the file "image1.pgm"
+  // h and w were passed by reference and now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  
+  // Now we can invert the image
+  int out[MAX_H][MAX_W];
+
 int box();
 int frame();
 int half();
