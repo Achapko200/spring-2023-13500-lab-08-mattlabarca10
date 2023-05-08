1a6727bbfbac1c055992d1dc3e8d13cefc461530
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2023-05-08 08:19:28 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include "invert.h"
-#include "invert-half.h"
-#include "box.h"
-#include "frame.h"
-#include "scale.h"
-#include "pixelate.h"
-#include "kernel.h"
+#include "box.h
 
 int main(){
     invert();
